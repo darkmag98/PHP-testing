1539a367cdd1ee66ad6df989c4a7b6336c18e152
Jamal Derani <darkmag98@nickfost.com>
2013-01-16 21:17:01 -0800

    Calculator

---
+++ calculator testing.php
@@ -0,0 +1,28 @@
+<?php
+  
+	
+	//You may change anything in this code, it was just for testing purposes :) 
+	
+	
+	//Addition
+	$math1 = 3 + 4;
+		echo $math1 . "<br/>";
+	
+	//Subtraction
+	$math2 = 3 - 4;
+		echo $math2. "<br/>";
+		
+	//Multiplication
+		$math3 = 3 * 4;
+			echo $math3. "<br/>";
+			
+	//Division
+		$math4 = 3 / 4;
+			echo $math4. "<br/>";
+			
+	//Modulo - gives remainder
+		$math5 = 4%1;
+			echo $math5. "<br/>";
+			
+
+?>
